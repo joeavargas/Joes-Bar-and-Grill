9c86b104df5a3a67dcdafbc20d7e42447b4c6d13
Joe Vargas <joeavargas@outlook.com>
2021-06-15 11:00:27 -0500

    Update YourOrder badge count function to remove the badge when order count is 0

--- Delegates/SceneDelegate.swift
+++ Delegates/SceneDelegate.swift
@@ -13,7 +13,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var orderTabBarItem: UITabBarItem!
     
     @objc func updateOrderBadge(){
-        orderTabBarItem.badgeValue = String(MenuController.shared.order.menuItems.count)
+        
+        switch MenuController.shared.order.menuItems.count {
+        case 0:
+            orderTabBarItem.badgeValue = nil
+        case let count:
+            orderTabBarItem.badgeValue = String(count)
+        }
     }
 
 
